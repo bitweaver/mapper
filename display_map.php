6d956a6f21befaf195c2b6f76b91b1812025407e
wjames5 <will@tekimaki.com>
2009-10-01 14:17:02 +0000

    remove ref to non-existant copyright.txt file

--- display_map.php
+++ display_map.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_mapper/display_map.php,v 1.5 2009/10/01 13:45:44 wjames5 Exp $
+ * $Header: /cvsroot/bitweaver/_bit_mapper/display_map.php,v 1.6 2009/10/01 14:17:02 wjames5 Exp $
  *
  * Copyright ( c ) 2004 bitweaver.org
- * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * All Rights Reserved. See below for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details
  *
  * @package mapper
